fa3cb91507aafdc05f9cc8ae293982703d966684
Pedro Fernandes <pedropbfernandes@gmail.com>
2020-06-17 14:59:20 -0300

    Teste de unidade de código C# no .net usando dotnet e xUnit

---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prime.Service
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            throw new NotImplementedException("Not implemented.");
+        }
+    }
+}
